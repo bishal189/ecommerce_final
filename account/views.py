982cde2818cf8d326b0fca179202ad0de0c9ccc3
bishal189 <bishalmurmu150@gmail.com>
2024-03-07 15:00:30 +0545

    updated to handle proper photo and messages error if there is form error

--- account/views.py
+++ account/views.py
@@ -31,20 +31,22 @@ def register(request):
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
             username = email.split("@")[0]
+            #check if there is already an email associated
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
+        
 
             # Create a user profile
             profile = UserProfile()
             profile.user_id = user.id
-            profile.profile_picture = 'default/default-user.png'
+            profile.profile_picture = 'userprofile/EDV_lottery_photo.jpg'
             profile.save()
 
             # USER ACTIVATION
             current_site = get_current_site(request)
             mail_subject = 'Please activate your account'
-            message = render_to_string('accounts/account_verification_email.html', {
+            message = render_to_string('accounts/account_verfication_email.html', {
                 'user': user,
                 'domain': current_site,
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
@@ -53,10 +55,16 @@ def register(request):
             to_email = email
             send_email = EmailMessage(mail_subject, message, to=[to_email])
             send_email.send()
-            messages.success(request, 'Thank you for registering with us. We have sent you a verification email to your email address [rathan.kumar@gmail.com]. Please verify it.')
-            return redirect('/accounts/login/?command=verification&email='+email)
+            messages.success(request, f'Thank you for registering with us. We have sent you a verification email to your email address {to_email}. Please verify it.')
+            print("upto redirect")
+            return redirect('/account/login/')
+        else:
+            messages.error(request, f'Email  is already registered')
+
+            
     else:
         form = RegistrationForm()
+
     context = {
         'form': form,
     }
