b4c4574a4577c04c6d7d721c521967fae2df0276
sandeshsitaula <navaltajohan@gmail.com>
2024-03-08 08:51:14 +0545

    publish subscribe model completed"

--- store/signals.py
+++ store/signals.py
@@ -7,18 +7,18 @@ from django.contrib.auth.models import User
 from .models import Product
 from account.models import Account
 from django.core.mail import EmailMessage
-
+from subscribe.models import SubscribeModel
 @receiver(post_save, sender=Product)
 def send_notification_to_users(sender, instance, created, **kwargs):
-    print("some notification")
     if created:
-        print("hello")
         subject = 'New Product Added'
-        message = f"A new item '{instance.product_name}' has been added in greatStore. Check it out!"
-        users = Account.objects.filter(is_active=True)
+        message = f"A new item '{instance.product_name}' has been added in category {instance.category.category_name} in our greatStore. Check it out!"
+
+        users =SubscribeModel.objects.get(category=instance.category)
         print(users)
-        for user in users:
+        for user in users.subscribers.all():
             to_email=user.email
+            print(to_email)
             send_email = EmailMessage(subject, message, to=[to_email])
             send_email.send()
 
