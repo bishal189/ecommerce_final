f34d2f73d0e878399e8790f8b1d3ddb0cdd47513
bishal189 <bishalmurmu150@gmail.com>
2024-03-13 10:34:19 +0545

    making the address line 2 field optionals

--- cart/models.py
+++ cart/models.py
@@ -70,7 +70,7 @@ class Order(models.Model):
     phone=models.CharField(max_length=20)
     email=models.EmailField(max_length=50)
     address_line_1=models.CharField(max_length=100)
-    address_line_2=models.CharField(max_length=100)
+    address_line_2=models.CharField(max_length=100,blank=True,null=True)
     country=models.CharField(max_length=50)
     state=models.CharField(max_length=50)
     city=models.CharField(max_length=20)
