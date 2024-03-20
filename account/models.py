2fa17d87e96633a4a80a15b9b09decc98a082f6e
bishal189 <bishalmurmu150@gmail.com>
2024-03-20 20:53:21 +0545

    added models for logo

--- account/models.py
+++ account/models.py
@@ -107,3 +107,9 @@ class UserProfile(models.Model):
 
     def full_address(self):
         return f'{self.address_line_1} {self.address_line_2}'
+
+
+class Logo(models.Model):
+    category_image=models.ImageField(upload_to='photos/logo',blank=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
